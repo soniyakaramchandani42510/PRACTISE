d6c71c241b5ab393e3d5b7293e0b9ee73a96a953
Pushker-stark <pushkernaresh0607@gmail.com>
2021-09-06 17:51:53 +0530

    two Chidiyan addedgit st

--- Home/home.js
+++ Home/home.js
@@ -1,3 +1,24 @@
+// Play initial animations on page load.
+$window.on('load', function() {
+    window.setTimeout(function() {
+        $body.removeClass('is-preload');
+    }, 100);
+});
+
+// Header.
+if ($banner.length > 0
+&&	$header.hasClass('alt')) {
+
+    $window.on('resize', function() { $window.trigger('scroll'); });
+
+    $banner.scrollex({
+        bottom:		$header.outerHeight(),
+        terminate:	function() { $header.removeClass('alt'); },
+        enter:		function() { $header.addClass('alt'); },
+        leave:		function() { $header.removeClass('alt'); }
+    });
+
+}
 //Get the button
 var mybutton = document.getElementById("back-to-top");
 
